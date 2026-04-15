24b7e91e391b5dae158625b7212276e86bedbcba
Natália Pereira <78566867+natybastosp@users.noreply.github.com>
2026-04-15 13:27:54 -0300

    feat: integrate Tailwind CSS and remove legacy styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1 class="text-4xl font-bold underline">Welcome to My MacFolio</h1>
+    </div>
+  );
 };
 
 export default App;
